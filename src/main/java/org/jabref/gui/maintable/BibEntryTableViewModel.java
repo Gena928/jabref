306a626a1e4ae05fc307a0d309d8690cefbdeb74
Gena928 <GennadiyStakhovskiy@gmail.com>
2020-05-02 23:05:34 +0300

    Cleared .idea folder and created settings again

--- src/main/java/org/jabref/gui/maintable/BibEntryTableViewModel.java
+++ src/main/java/org/jabref/gui/maintable/BibEntryTableViewModel.java
@@ -111,6 +111,7 @@ public class BibEntryTableViewModel {
 
     public ObservableValue<String> getFields(OrFields fields) {
         ObservableValue<String> value = fieldValues.get(fields);
+        String checking = fields.getDisplayName();
         if (value != null) {
             return value;
         }
