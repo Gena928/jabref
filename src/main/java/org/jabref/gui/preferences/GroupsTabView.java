86a3a96fc4545b84b4f3e82cfc9c044fa627d68c
Gennadiy Stakhovskiy <GennadiyStakhovskiy@gmail.com>
2020-09-29 11:16:21 +0300

    Fixed 6867 - double key binding (ctrl+shift+R for pulling changes from shared database and creating tech report).

--- src/main/java/org/jabref/gui/preferences/GroupsTabView.java
+++ src/main/java/org/jabref/gui/preferences/GroupsTabView.java
@@ -6,19 +6,22 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 import org.jabref.logic.l10n.Localization;
-import org.jabref.preferences.PreferencesService;
+import org.jabref.preferences.JabRefPreferences;
 
 import com.airhacks.afterburner.views.ViewLoader;
 
 public class GroupsTabView extends AbstractPreferenceTabView<GroupsTabViewModel> implements PreferencesTab {
 
+    @FXML private CheckBox grayNonHits;
     @FXML private RadioButton groupViewModeIntersection;
     @FXML private RadioButton groupViewModeUnion;
     @FXML private CheckBox autoAssignGroup;
-    @FXML private CheckBox displayGroupCount;
+    @FXML private CheckBox displayGroupQuantity;
+    @FXML private TextField defaultGroupingField;
     @FXML private TextField keywordSeparator;
 
-    public GroupsTabView(PreferencesService preferences) {
+
+    public GroupsTabView(JabRefPreferences preferences) {
         this.preferences = preferences;
 
         ViewLoader.view(this)
@@ -27,17 +30,17 @@ public class GroupsTabView extends AbstractPreferenceTabView<GroupsTabViewModel>
     }
 
     @Override
-    public String getTabName() {
-        return Localization.lang("Groups");
-    }
+    public String getTabName() { return Localization.lang("Groups"); }
 
     public void initialize() {
         this.viewModel = new GroupsTabViewModel(dialogService, preferences);
 
+        grayNonHits.selectedProperty().bindBidirectional(viewModel.grayNonHitsProperty());
         groupViewModeIntersection.selectedProperty().bindBidirectional(viewModel.groupViewModeIntersectionProperty());
         groupViewModeUnion.selectedProperty().bindBidirectional(viewModel.groupViewModeUnionProperty());
         autoAssignGroup.selectedProperty().bindBidirectional(viewModel.autoAssignGroupProperty());
-        displayGroupCount.selectedProperty().bindBidirectional(viewModel.displayGroupCount());
+        displayGroupQuantity.selectedProperty().bindBidirectional(viewModel.displayGroupQuantity());
+        defaultGroupingField.textProperty().bindBidirectional(viewModel.defaultGroupingFieldProperty());
         keywordSeparator.textProperty().bindBidirectional(viewModel.keywordSeparatorProperty());
     }
 }
--- src/main/java/org/jabref/gui/preferences/GroupsTabViewModel.java
+++ src/main/java/org/jabref/gui/preferences/GroupsTabViewModel.java
@@ -10,86 +10,78 @@ import javafx.beans.property.StringProperty;
 
 import org.jabref.gui.DialogService;
 import org.jabref.gui.groups.GroupViewMode;
-import org.jabref.gui.groups.GroupsPreferences;
-import org.jabref.preferences.PreferencesService;
+import org.jabref.preferences.JabRefPreferences;
 
 public class GroupsTabViewModel implements PreferenceTabViewModel {
 
+    private final BooleanProperty grayNonHitsProperty = new SimpleBooleanProperty();
     private final BooleanProperty groupViewModeIntersectionProperty = new SimpleBooleanProperty();
     private final BooleanProperty groupViewModeUnionProperty = new SimpleBooleanProperty();
     private final BooleanProperty autoAssignGroupProperty = new SimpleBooleanProperty();
-    private final BooleanProperty displayGroupCountProperty = new SimpleBooleanProperty();
+    private final BooleanProperty displayGroupQuantityProperty = new SimpleBooleanProperty();
+    private final StringProperty defaultGroupingFieldProperty = new SimpleStringProperty("");
     private final StringProperty keywordSeparatorProperty = new SimpleStringProperty("");
 
     private final DialogService dialogService;
-    private final PreferencesService preferences;
-    private final GroupsPreferences initialGroupsPreferences;
+    private final JabRefPreferences preferences;
 
-    public GroupsTabViewModel(DialogService dialogService, PreferencesService preferences) {
+    public GroupsTabViewModel(DialogService dialogService, JabRefPreferences preferences) {
         this.dialogService = dialogService;
         this.preferences = preferences;
-        this.initialGroupsPreferences = preferences.getGroupsPreferences();
     }
 
     @Override
     public void setValues() {
-        switch (initialGroupsPreferences.getGroupViewMode()) {
-            case INTERSECTION -> {
+        grayNonHitsProperty.setValue(preferences.getBoolean(JabRefPreferences.GRAY_OUT_NON_HITS));
+        switch (preferences.getGroupViewMode()) {
+            case INTERSECTION:
                 groupViewModeIntersectionProperty.setValue(true);
                 groupViewModeUnionProperty.setValue(false);
-            }
-            case UNION -> {
+                break;
+            case UNION:
                 groupViewModeIntersectionProperty.setValue(false);
                 groupViewModeUnionProperty.setValue(true);
-            }
+                break;
         }
-        autoAssignGroupProperty.setValue(initialGroupsPreferences.shouldAutoAssignGroup());
-        displayGroupCountProperty.setValue(initialGroupsPreferences.shouldDisplayGroupCount());
-        keywordSeparatorProperty.setValue(initialGroupsPreferences.getKeywordDelimiter().toString());
+        autoAssignGroupProperty.setValue(preferences.getBoolean(JabRefPreferences.AUTO_ASSIGN_GROUP));
+        displayGroupQuantityProperty.setValue(preferences.getBoolean(JabRefPreferences.DISPLAY_GROUP_QUANTITY));
+
+        defaultGroupingFieldProperty.setValue(preferences.get(JabRefPreferences.GROUPS_DEFAULT_FIELD));
+        keywordSeparatorProperty.setValue(preferences.get(JabRefPreferences.KEYWORD_SEPARATOR));
     }
 
     @Override
     public void storeSettings() {
-        GroupViewMode groupViewMode = GroupViewMode.UNION;
+        preferences.putBoolean(JabRefPreferences.GRAY_OUT_NON_HITS, grayNonHitsProperty.getValue());
         if (groupViewModeIntersectionProperty.getValue()) {
-            groupViewMode = GroupViewMode.INTERSECTION;
+            preferences.setGroupViewMode(GroupViewMode.INTERSECTION);
+        } else if (groupViewModeUnionProperty.getValue()) {
+            preferences.setGroupViewMode(GroupViewMode.UNION);
         }
+        preferences.putBoolean(JabRefPreferences.AUTO_ASSIGN_GROUP, autoAssignGroupProperty.getValue());
+        preferences.putBoolean(JabRefPreferences.DISPLAY_GROUP_QUANTITY, displayGroupQuantityProperty.getValue());
 
-        GroupsPreferences newGroupsPreferences = new GroupsPreferences(
-                groupViewMode,
-                autoAssignGroupProperty.getValue(),
-                displayGroupCountProperty.getValue(),
-                keywordSeparatorProperty.getValue().charAt(0));
-        preferences.storeGroupsPreferences(newGroupsPreferences);
+        preferences.put(JabRefPreferences.GROUPS_DEFAULT_FIELD, defaultGroupingFieldProperty.getValue().trim());
+        preferences.put(JabRefPreferences.KEYWORD_SEPARATOR, keywordSeparatorProperty.getValue());
     }
 
     @Override
-    public boolean validateSettings() {
-        return true;
-    }
+    public boolean validateSettings() { return true; }
 
     @Override
-    public List<String> getRestartWarnings() {
-        return new ArrayList<>();
-    }
+    public List<String> getRestartWarnings() { return new ArrayList<>(); }
 
-    public BooleanProperty groupViewModeIntersectionProperty() {
-        return groupViewModeIntersectionProperty;
-    }
+    public BooleanProperty grayNonHitsProperty() { return grayNonHitsProperty; }
 
-    public BooleanProperty groupViewModeUnionProperty() {
-        return groupViewModeUnionProperty;
-    }
+    public BooleanProperty groupViewModeIntersectionProperty() { return groupViewModeIntersectionProperty; }
 
-    public BooleanProperty autoAssignGroupProperty() {
-        return autoAssignGroupProperty;
-    }
+    public BooleanProperty groupViewModeUnionProperty() { return groupViewModeUnionProperty; }
 
-    public BooleanProperty displayGroupCount() {
-        return displayGroupCountProperty;
-    }
+    public BooleanProperty autoAssignGroupProperty() { return autoAssignGroupProperty; }
 
-    public StringProperty keywordSeparatorProperty() {
-        return keywordSeparatorProperty;
-    }
+    public BooleanProperty displayGroupQuantity() { return displayGroupQuantityProperty;}
+
+    public StringProperty defaultGroupingFieldProperty() { return defaultGroupingFieldProperty; }
+
+    public StringProperty keywordSeparatorProperty() { return keywordSeparatorProperty; }
 }
