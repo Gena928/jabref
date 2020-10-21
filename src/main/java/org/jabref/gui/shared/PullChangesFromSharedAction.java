aaf86eff3f3844818f45e93333c30da7a3e6e01a
Gennadiy Stakhovskiy <GennadiyStakhovskiy@gmail.com>
2020-10-21 21:24:54 +0300

    Revert "Additiona filter on execute() method, to ensure we are connected to shared database."
    This reverts commit cc2f9d89

--- src/main/java/org/jabref/gui/shared/PullChangesFromSharedAction.java
+++ src/main/java/org/jabref/gui/shared/PullChangesFromSharedAction.java
@@ -3,7 +3,6 @@ package org.jabref.gui.shared;
 import org.jabref.gui.StateManager;
 import org.jabref.gui.actions.ActionHelper;
 import org.jabref.gui.actions.SimpleCommand;
-import org.jabref.logic.shared.DatabaseLocation;
 import org.jabref.logic.shared.DatabaseSynchronizer;
 
 public class PullChangesFromSharedAction extends SimpleCommand {
@@ -17,9 +16,7 @@ public class PullChangesFromSharedAction extends SimpleCommand {
     }
 
     public void execute() {
-        stateManager.getActiveDatabase()
-                    .filter(databaseContext->databaseContext.getLocation() == DatabaseLocation.SHARED)
-                    .ifPresent(databaseContext -> {
+        stateManager.getActiveDatabase().ifPresent(databaseContext -> {
             DatabaseSynchronizer dbmsSynchronizer = databaseContext.getDBMSSynchronizer();
             dbmsSynchronizer.pullChanges();
         });
